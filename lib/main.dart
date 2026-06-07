750804e62d3099285acbf6cf81af6493d83ac994
daniellegy <le202332796@alm.buap.mx>
2026-06-06 21:20:30 -0600

    feat: Agrego la url de la api backend a un archivo .env en la carpeta raiz

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 // Cambiamos la importación para usar el provider
 import 'core/routing/app_router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const ProviderScope(child: RescueNetApp()));
 }
 
