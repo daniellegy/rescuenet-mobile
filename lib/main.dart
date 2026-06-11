b93e9aa431ba453b9e51d646430bf7af121def5e
daniellegy <le202332796@alm.buap.mx>
2026-06-11 17:53:25 -0600

    feat: Importamos firebase en main y lo inicializamos

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-// Cambiamos la importación para usar el provider
 import 'core/routing/app_router.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp();
   runApp(const ProviderScope(child: RescueNetApp()));
 }
 
