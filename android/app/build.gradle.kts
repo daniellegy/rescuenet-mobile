759eb846ff201e1d7d4ec69dc1809dbc4fad1d71
daniellegy <le202332796@alm.buap.mx>
2026-06-11 18:36:13 -0600

    feat: Modificamos las credenciales de firebase para que funcionen correctamente

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "mx.buap.rescuenet.rescuenet_mobile"
+        applicationId = "com.latidotermico.rescuenet"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
