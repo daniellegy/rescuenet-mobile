70004fa983ff441213a73f80a41066d29d3ea438
daniellegy <le202332796@alm.buap.mx>
2026-08-20 23:42:13 -0600

    fix: Solucionar ruta del banner de rescate activo al stepper

--- lib/features/map/presentation/widgets/active_rescue_card.dart
+++ lib/features/map/presentation/widgets/active_rescue_card.dart
@@ -17,7 +17,7 @@ class ActiveRescueCard extends ConsumerWidget {
 
     return GestureDetector(
       onTap: () {
-        context.push('/report-stepper', extra: rescate).then((_) {
+        context.push('/rescue-stepper', extra: rescate).then((_) {
           ref.invalidate(reportesActivosMapaProvider);
           ref.invalidate(miRescateActivoProvider);
         });
